59de058c644eb6cab9350fc08903a94b92dfa8bf
jtguo <jtguo2@iflytek.com>
2024-04-01 18:27:24 +0800

    添加Analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import { ThemeProvider } from "@/components/theme-provider";
 import Navbar from "@/components/navbar";
 import "./globals.css";
 import { SpeedInsights } from "@vercel/speed-insights/next"
+import { Analytics } from '@vercel/analytics/react';
 
 export const metadata: Metadata = {
   title: "Jet Kwok",
@@ -48,6 +49,7 @@ export default function RootLayout({
           <main className="mb-10">{children}</main>
         </ThemeProvider>
         <SpeedInsights />
+        <Analytics />
       </body>
     </html>
   );
